7f1032d5eb8955e9d1af11d8115b025631ed6f5d
jedsada <jt@20scoops.com>
2016-12-17 14:25:28 +0700

    test unit_test

---
+++ app/src/main/java/com/wisdomlanna/www/kotlin/Calculate.kt
@@ -0,0 +1,11 @@
+package com.wisdomlanna.www.kotlin
+
+/**
+ * Created by jedsada on 12/17/16.
+ */
+object Calculate {
+
+    fun plus(x: Int, y: Int): Int {
+        return x + y
+    }
+}
---
+++ app/src/test/java/com/wisdomlanna/www/kotlin/CalculateTest.kt
@@ -0,0 +1,14 @@
+package com.wisdomlanna.www.kotlin
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Created by jedsada on 12/17/16.
+ */
+class CalculateTest {
+    @Test
+    fun plus() {
+        assertEquals(5, Calculate.plus(5, 90))
+    }
+}
